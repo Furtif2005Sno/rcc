5a1c3f97260c1c49523f5cbdc17ab890b9da94af
Jolan Noirot <jolan.noirot@laposte.net>
2024-12-08 12:49:35 +0000

    page de connexion

--- Exclusivité.php
+++ Exclusivité.php
@@ -1,14 +1,79 @@
-/* From Uiverse.io by bociKond */ 
-<form class="form">
-  <span class="input-span">
-    <label for="email" class="label">Email</label>
-    <input type="email" name="email" id="email"
-  /></span>
-  <span class="input-span">
-    <label for="password" class="label">Password</label>
-    <input type="password" name="password" id="password"
-  /></span>
-  <span class="span"><a href="#">Forgot password?</a></span>
-  <input class="submit" type="submit" value="Log in" />
-  <span class="span">Don't have an account? <a href="#">Sign up</a></span>
-</form>
+<<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Rent Connect</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/portfolio.png" rel="icon">
+    <link href="assets/img/portfolio.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/Exclusivité.css" rel="stylesheet">
+    <script src="assets/js/main.js"></script>
+    <script src="js/script.js"></script>
+    <script src="https://kit.fontawesome.com/cc37040a86.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        /* From Uiverse.io by bociKond */ 
+        <form class="form">
+        <span class="input-span">
+            <label for="email" class="label">Email</label>
+            <input type="email" name="email" id="email"
+        /></span>
+        <span class="input-span">
+            <label for="password" class="label">Password</label>
+            <input type="password" name="password" id="password"
+        /></span>
+        <span class="span"><a href="#">Forgot password?</a></span>
+        <input class="submit" type="submit" value="Log in" />
+        <span class="span">Don't have an account? <a href="#">Sign up</a></span>
+        </form>
+
+        
+        <div id="preloader"></div>
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+        <!-- Vendor JS Files -->
+        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="assets/vendor/aos/aos.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/vendor/typed.js/typed.umd.js"></script>
+        <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="assets/js/main.js"></script>
+        <script src="" async defer></script>
+    </body>
+</html>
