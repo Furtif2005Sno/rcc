65ecc289270a92f2ba6b920e32d14f80ba290bb1
Jolan Noirot <jolan.noirot@laposte.net>
2024-12-09 21:48:59 +0000

    style

--- index.php
+++ index.php
@@ -197,41 +197,41 @@
               <span class="visually-hidden">Suivant</span>
             </button>
           </div>
-
-          <!-- Formulaire pour ajouter un avis -->
-          <div class="mt-4 justify-content-center">
-            <h3>Ajouter un avis</h3>
-            <form action="add-avis.php" method="POST">
-              <div class="mb-3 form__group field">
-                <select name="voiture_id" id="voiture_id" class="form__field" required>
-                  <?php
-                  $sql = "SELECT id, marque, modele FROM voitures";
-                  $result = $db1->query($sql);
-                  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<option value="' . htmlspecialchars($row['id']) . '">' . htmlspecialchars($row['marque']) . ' ' . htmlspecialchars($row['modele']) . '</option>';
-                  }
-                  ?>
-                </select>
-                <label for="voiture_id" class="form__label">Voiture</label>
-              </div>
-              <div class="mb-3 form__group field">
-                <!--<label for="note" class="form-label">Note (sur 5)</label>
-                <input type="number" name="note" id="note" class="form-control" min="1" max="5" required>-->
-                <input type="number" class="form__field" placeholder="Note" name="note" id="note" min="1" max="5"
-                  required />
-                <label for="name" class="form__label">Note (sur 5)</label>
-              </div>
-              <div class="mb-3 form__group field">
-                <!--<label for="commentaire" class="form-label">Commentaire</label>
-                <textarea name="commentaire" id="commentaire" class="form-control" rows="4" required></textarea>-->
-                <textarea type="input" class="form__field" placeholder="Commentaire" name="commentaire" id='commentaire'
-                  required></textarea>
-                <label for="name" class="form__label">Commentaire</label>
-              </div>
-              <button type="submit" class="btn btn-outline-danger rounded-pill custom-danger2">Ajouter</button>
-            </form>
-          </div>
         </div>
+        <!-- Formulaire pour ajouter un avis -->
+        <div class="mt-4 justify-content-center">
+          <h3>Ajouter un avis</h3>
+          <form action="add-avis.php" method="POST">
+            <div class="mb-3 form__group field">
+              <select name="voiture_id" id="voiture_id" class="form__field" required>
+                <?php
+                $sql = "SELECT id, marque, modele FROM voitures";
+                $result = $db1->query($sql);
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                  echo '<option value="' . htmlspecialchars($row['id']) . '">' . htmlspecialchars($row['marque']) . ' ' . htmlspecialchars($row['modele']) . '</option>';
+                }
+                ?>
+              </select>
+              <label for="voiture_id" class="form__label">Voiture</label>
+            </div>
+            <div class="mb-3 form__group field">
+              <!--<label for="note" class="form-label">Note (sur 5)</label>
+                <input type="number" name="note" id="note" class="form-control" min="1" max="5" required>-->
+              <input type="number" class="form__field" placeholder="Note" name="note" id="note" min="1" max="5"
+                required />
+              <label for="name" class="form__label">Note (sur 5)</label>
+            </div>
+            <div class="mb-3 form__group field">
+              <!--<label for="commentaire" class="form-label">Commentaire</label>
+                <textarea name="commentaire" id="commentaire" class="form-control" rows="4" required></textarea>-->
+              <textarea type="input" class="form__field" placeholder="Commentaire" name="commentaire" id='commentaire'
+                required></textarea>
+              <label for="name" class="form__label">Commentaire</label>
+            </div>
+            <button type="submit" class="btn btn-outline-danger rounded-pill custom-danger2">Ajouter</button>
+          </form>
+        </div>
+
       </div>
     </section>
     <!-- End Resume Section -->
@@ -260,11 +260,9 @@
               </select>
               <label for="marque" class="form__label">Marque</label>
             </div>
-            <div class="row g-3 mb-4">
-              <div class="col-md-4 form__group field">
-                <input type="text" id="modele" name="modele" class="form__field" placeholder="Ex : Clio">
-                <label for="modele" class="form__label">Modèle</label>
-              </div>
+            <div class="col-md-4 form__group field">
+              <input type="text" id="modele" name="modele" class="form__field" placeholder="Ex : Clio">
+              <label for="modele" class="form__label">Modèle</label>
             </div>
             <div class="col-md-4 form__group field">
               <input type="number" id="prix_min" name="prix_min" class="form__field" placeholder="€">
@@ -275,7 +273,7 @@
               <label for="prix_max" class="form__label">Prix (Max)</label>
             </div>
           </div>
-          <div class="row g-3 mb-4">
+          <div class="row g-3 mb-4 justify-content-center">
             <div class="col-md-4 align-self-end">
               <button type="submit" class="btn btn-outline-danger rounded-pill custom-danger2">Filtrer</button>
             </div>
